d72afa00518dfc035a432162c03cd7baeb90bfab
Gustavo Figuera <processum1@gmail.com>
2023-01-23 22:29:37 +0000

    add: App odoo_academy foundation

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,14 @@
+# * -*- coding: utf-8 -*-
+{
+    'name': 'DIMAC21 Academy',
+    'summary': 'Primer módulo de desarrollo SH para DIMAC21',
+    'description': 'Módulo de desarrollo siguiendo el tutorial de Odoo Technical Training',
+    'author': 'Soluciones DIMAC21',
+    'category': 'DIMAC21',
+    'version': '0.1',
+    'website': 'https://www.dimac21.com',
+    'depends': [
+        'base'
+    ],
+    'application': True
+}
